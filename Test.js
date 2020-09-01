7aa557105d339d1d5e2755a12b663efa97eab0fd
SnirGafni-shiratech <51399294+SnirGafni-shiratech@users.noreply.github.com>
2020-09-01 17:20:08 +0300

    Private functions

--- Test.js
+++ Test.js
@@ -12,28 +12,31 @@ var testRun = function(){
 	var parser = new iCoMoXParser();
 	var obj;
 	
+	console.log("parser:");
+	console.log(parser);
+	console.log(JSON.stringify(parser));
 	
 	//Hello Message
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Hello.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Hello.bin", null));
 	console.log("Hello message:" + JSON.stringify(obj));
 
 	//Report messages
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_ACC1_AXL362.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_ACC1_AXL362.bin", null));
 	console.log("Report - ACC1:" + JSON.stringify(obj));
 	
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_ACC2_AXL356.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_ACC2_AXL356.bin", null));
 	console.log("Report - ACC12" + JSON.stringify(obj));
 
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_MAG_BMM150.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_MAG_BMM150.bin", null));
 	console.log("Report - Manetometer:" + JSON.stringify(obj));
 
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_Temp_ADT7410_1.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_Temp_ADT7410_1.bin", null));
 	console.log("Report - Temperature:" + JSON.stringify(obj));
 
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_Temp_ADT7410_2.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_Temp_ADT7410_2.bin", null));
 	console.log("Report - Temperature" + JSON.stringify(obj));
 
-	obj = (parser.binaryDataSet(fs.readFileSync("Messages/Report_MIC_IM69D130.bin", null))).objectGet();
+	obj = parser.objectGet(fs.readFileSync("Messages/Report_MIC_IM69D130.bin", null));
 	console.log("Report - Microphone:" + JSON.stringify(obj));
 
 }
--- iCoMoXParser.js
+++ iCoMoXParser.js
@@ -4,109 +4,75 @@ module.exports = function iCoMoXParser(binaryData) {
 	const BOARD_TYPE = ["SMIP","NB_IOT","POE"];
 	const FW_BRANCH = ["Kit","Suitcase"];
 	
-	const MESSAGE_TYPE = {
-	  HELLO:0x00,
-	  REPORT:0xFF
-	  
-	};
+	
 
-	const REPORT_TYPE = {
-		ACCELEROMETER_1:0, 	//AXL362
-		ACCELEROMETER_2:1, 	//ADXL362
-		MAGNETOMETER:2,		//BMM150	
-		TEMP:3,				//ADT7410
-		MIC:4,			//IM69D130
-		//ACCELEROMETER_3:5,	//ADXL1002
-	};
-	
-	//Binary data set
-	this.binaryDataSet = function(binaryData) {	
-		if (Buffer.isBuffer(binaryData) == false){
-			console.log("Error:Not a buffer");
-			this.binaryData = null;
-			return null;
-		}	
-		this.binaryData = binaryData;
-		return this;		
-	}
-	this.messageTypeGet = function() {
-		switch (this.binaryData[0]){
-			//Report
-			case MESSAGE_TYPE.REPORT:
-				break;
-			case MESSAGE_TYPE.HELLO:
-				break;
-			default:
-				return null;
-		}
-		return this.binaryData[0];
-	}
-	
-	//Hello message
-	this.twoDigStr =  function(num){
+	//Utility functions
+	var twoDigStr =  function(num){
 		return ('0' + (num).toString(10)).slice(-2);
 	}
-	this.toHexStr =  function(num){
+	var toHexStr =  function(num){
 		return ('0' + (num).toString(16)).slice(-2);
 	}
 	
-	this.helloMessageGet = function() {
-		if (this.messageTypeGet()!=MESSAGE_TYPE.HELLO)
-			return null;
-		console.log("1");
+	//Message parsers - Hello
+	var helloMessageGet = function(binaryData) {
 		var res = {};
-		if (this.binaryData[1] >= BOARD_TYPE.length)
+		if (binaryData[1] >= BOARD_TYPE.length)
 			return null;
-		console.log("2");
-		if (this.binaryData[23] >= FW_BRANCH.length)
+		if (binaryData[23] >= FW_BRANCH.length)
 			return null;
-		console.log("3");
-		res["BoardType"] = BOARD_TYPE[this.binaryData[1]];
-		res["Board ver"] = this.binaryData[2] + "." + this.binaryData[3];
+		res["BoardType"] = BOARD_TYPE[binaryData[1]];
+		res["Board ver"] = binaryData[2] + "." + binaryData[3];
 		res["MCU Serial"]="";
 		for (var i=0; i< 16; i++)
-			res["MCU Serial"]+= this.toHexStr(this.binaryData[4+i]);   
-		res["FW ver"] = this.binaryData[20] + "." + this.binaryData[21] + "." + this.binaryData[22] + FW_BRANCH[this.binaryData[23]];
-		res["FW build"] = 	this.binaryData[27] + "."  + this.binaryData[26] + "." + this.binaryData.readInt16LE(24)  +   " " + this.binaryData[28] + ":" + this.twoDigStr(this.binaryData[29]) + ":" + this.twoDigStr(this.binaryData[30]);
-		res["BIT status"] = "0x" + this.toHexStr(this.binaryData[31]);
-		res["Part num"] = this.binaryData.slice(32,32+32).toString().replace(/\0[\s\S]*$/g,'');
-		res["Serial"] = this.binaryData.slice(64,64+32).toString().replace(/\0[\s\S]*$/g,'');
-		res["Name"] = this.binaryData.slice(96,96+32).toString().replace(/\0[\s\S]*$/g,'');
+			res["MCU Serial"]+= toHexStr(binaryData[4+i]);   
+		res["FW ver"] = binaryData[20] + "." + binaryData[21] + "." + binaryData[22] + FW_BRANCH[binaryData[23]];
+		res["FW build"] = 	binaryData[27] + "."  + binaryData[26] + "." + binaryData.readInt16LE(24)  +   " " + binaryData[28] + ":" + twoDigStr(binaryData[29]) + ":" + twoDigStr(binaryData[30]);
+		res["BIT status"] = "0x" + toHexStr(binaryData[31]);
+		res["Part num"] = binaryData.slice(32,32+32).toString().replace(/\0[\s\S]*$/g,'');
+		res["Serial"] = binaryData.slice(64,64+32).toString().replace(/\0[\s\S]*$/g,'');
+		res["Name"] = binaryData.slice(96,96+32).toString().replace(/\0[\s\S]*$/g,'');
 		
 		return res;
 	}
-	//Reports
-	this.reportTypeGet = function() {
-		if (this.isReportMessage()==false)
-			return null;
-		return this.binaryData[1];
-	}
-	this.isReportMessage = function() {		
-		return (this.messageTypeGet()==MESSAGE_TYPE.REPORT);
-	}
-	this.timestampGet = function() {
-		if ((this.binaryData.length < 10) && (this.isReportMessage()==true))
+	
+	//Message parsers - Reports
+	var timestampGet = function(binaryData) {
+		if ((binaryData.length < 10) && (this.isReportMessage()==true))
 			return null;		
 		
-		return new Date((Number(this.binaryData.readBigInt64LE(2) ) /32768) * 1000);
+		return new Date((Number(binaryData.readBigInt64LE(2) ) /32768) * 1000);
 		
 	}
+	var reportMessageGet = function(binaryData) {
+		var res = {};
+		
+		if (binaryData[1] >= REPORT_TYPE.length)
+			return null;
+		res.reportType = REPORT_TYPE[binaryData[1]].name;
+		res[res.reportType] = REPORT_TYPE[binaryData[1]].func(binaryData);
+		if (res[res.reportType] == null)
+			return null;
+		res.timestamp = timestampGet(binaryData);
+		
+		return res;
+	}
+	
+	
+	
 	
 	
 	//Reports - Accelerometer 1	
-	this.accelerometer1Get = function() {
-		if  (this.reportTypeGet()!=REPORT_TYPE.ACCELEROMETER_1)
-			return null;
-		
+	var accelerometer1Get = function(binaryData) {
 		//Number of samples per axis
-		var len = 	(this.binaryData.length - 10) / (3*2);	
+		var len = 	(binaryData.length - 10) / (3*2);	
 		var res = {X:new Float32Array(len),Y:new Float32Array(len),Z:new Float32Array(len)};
 		
 		 
 		for (var i=0; i < len; i++){
-			res.X[i] = this.binaryData.readInt16LE(10 + i*6) / 1024;
-			res.Y[i] = this.binaryData.readInt16LE(12 + i*6) / 1024;
-			res.Z[i] = this.binaryData.readInt16LE(14 + i*6) / 1024;
+			res.X[i] = binaryData.readInt16LE(10 + i*6) / 1024;
+			res.Y[i] = binaryData.readInt16LE(12 + i*6) / 1024;
+			res.Z[i] = binaryData.readInt16LE(14 + i*6) / 1024;
 		}
 		
 		return res;		
@@ -114,19 +80,16 @@ module.exports = function iCoMoXParser(binaryData) {
 	
 	
 	//Reports - Accelerometer 2	
-	this.accelerometer2Get = function() {
-		if  (this.reportTypeGet()!=REPORT_TYPE.ACCELEROMETER_2)
-			return null;
-		
+	var accelerometer2Get = function(binaryData) {
 		//Total number of samples
-		var len = 	Math.floor(2 * (this.binaryData.length - 10) / 3);	
+		var len = 	Math.floor(2 * (binaryData.length - 10) / 3);	
 		var tempArr = [new Float32Array(len / 3),new Float32Array(len / 3),new Float32Array(len / 3)];
 		
 		var payloadIndex = 10;
 		for (var i=0; i < len ; i = i+2){
 			//Get Raw data
-			tempArr[i  % 3][Math.floor(i / 3)] = ((this.binaryData[payloadIndex + 1] & 0x0F) << 8) | this.binaryData[payloadIndex];          
-			tempArr[(i + 1) % 3][ Math.floor((i+1) / 3)] =  (this.binaryData[payloadIndex + 2] << 4) | (this.binaryData[payloadIndex + 1] >> 4 )
+			tempArr[i  % 3][Math.floor(i / 3)] = ((binaryData[payloadIndex + 1] & 0x0F) << 8) | binaryData[payloadIndex];          
+			tempArr[(i + 1) % 3][ Math.floor((i+1) / 3)] =  (binaryData[payloadIndex + 2] << 4) | (binaryData[payloadIndex + 1] >> 4 )
 			payloadIndex = payloadIndex + 3;
 		}
 		
@@ -141,101 +104,75 @@ module.exports = function iCoMoXParser(binaryData) {
 	}
 	
 	//Reports - Magnetometer
-	this.magnetometerGet = function() {
-		if  (this.reportTypeGet()!=REPORT_TYPE.MAGNETOMETER)
-			return null;
-		
-		var len = 	Math.floor( (this.binaryData.length - 10) / 2);			
+	var magnetometerGet = function(binaryData) {
+		var len = 	Math.floor( (binaryData.length - 10) / 2);			
 		var tempArr = [new Int32Array(len / 3),new Int32Array(len / 3),new Int32Array(len / 3)];
 		
 		for (var i=0; i < len  ; i++){
-			tempArr[i  % 3][Math.floor(i / 3)] = 1000* this.binaryData.readInt16LE(10 + i*2) / 16;						
+			tempArr[i  % 3][Math.floor(i / 3)] = 1000* binaryData.readInt16LE(10 + i*2) / 16;						
 		}
 		
 		return {X:tempArr[0], Y:tempArr[1], Z:tempArr[2]};
-		
-		
 	}
 	
 	
 	//Reports - Temperature
-	this.temperatureGet = function() {
-		if  (this.reportTypeGet()!=REPORT_TYPE.TEMP)
-			return null;
-		return this.binaryData.readInt16LE(10)/128;
-	}
-	
+	var temperatureGet = function(binaryData) {
+		return binaryData.readInt16LE(10)/128;
+	}	
 	
 	//Reports - MIC
-	this.micGet = function() {
-		if  (this.reportTypeGet()!=REPORT_TYPE.MIC)
-			return null;
-		
-		var len = 	Math.floor( (this.binaryData.length - 10) / 2);			
+	var micGet = function(binaryData) {
+		var len = 	Math.floor( (binaryData.length - 10) / 2);			
 		var res =  new Float32Array(len);
 		
 		for (var i=0; i < len  ; i++){
-			res[i] = this.binaryData.readInt16LE(10 + i*2) * Math.pow(10, (130/20))/32768;						
+			res[i] = binaryData.readInt16LE(10 + i*2) * Math.pow(10, (130/20))/32768;						
 		}
 		
 		return res;
 	}
 	
-  
-	
+	//Parsing map
+	const MESSAGE_TYPE = {
+	  HELLO:{code:0x00, name:"Hello",  func:helloMessageGet },
+	  REPORT:{code:0xFF, name:"Report",  func:reportMessageGet  }
+	  
+	};
+
+	const REPORT_TYPE = [
+		{name:"ACC1", 	func:accelerometer1Get} ,	//AXL362
+		{name:"ACC2", 	func:accelerometer2Get} , 	//ADXL362
+		{name:"MAG", 	func:magnetometerGet} ,	//BMM150	
+		{name:"Temp", 	func:temperatureGet} ,		//ADT7410
+		{name:"MIC", 	func:micGet} ,				//IM69D130
+	];
 	
 	
 	//Get parsed object
-	this.objectGet = function() {
+	this.objectGet = function(binaryData) {
 		var res = {};
 		
-		//Parse only message type
-		if (this.messageTypeGet() == null)
+		if ((Buffer.isBuffer(binaryData) == false) || (binaryData.length < 1))
 			return null;
-		
-		switch (this.messageTypeGet()){
-			case MESSAGE_TYPE.REPORT:
-				res.type = "Report";
-				res.timestamp = this.timestampGet();
-				switch (this.reportTypeGet()){
-					case REPORT_TYPE.ACCELEROMETER_1:
-						res.reportType = "ACC1";
-						res[res.reportType] = this.accelerometer1Get();						
-						break;
-					case REPORT_TYPE.ACCELEROMETER_2:
-						res.reportType = "ACC2";
-						res[res.reportType] = this.accelerometer2Get();
-						break;
-					case REPORT_TYPE.MAGNETOMETER:
-						res.reportType = "MAG";
-						res[res.reportType] = this.magnetometerGet();
-						break;
-					case REPORT_TYPE.TEMP:
-						res.reportType = "Temp";
-						res[res.reportType] = this.temperatureGet();
-						break;
-					case REPORT_TYPE.MIC:
-						res.reportType = "MIC";
-						res[res.reportType] = this.micGet();
-						break;
-					default:
-						return null;
-				}
+		var msgType;
+		switch (binaryData[0]){ //Message type
+			case MESSAGE_TYPE.REPORT.code:
+				msgType = MESSAGE_TYPE.REPORT;
 				break;
-			case MESSAGE_TYPE.HELLO:				
-				res = this.helloMessageGet();
-				if (res == null)
-					return null;
-				res.type = "Hello";				
-			break;			
+			case MESSAGE_TYPE.HELLO.code:
+				msgType = MESSAGE_TYPE.HELLO;
+				break;
 			default:
 				return null;
 		}
-		return res;
+		console.log("msg type:"+ JSON.stringify(msgType));
+		res = msgType.func(binaryData);
+		if (res == null)
+			return null;
+		res["type"] = msgType.name;
+		return res;		
 	}
 	
-	//Set data
-	if (binaryData != undefined)
-		this.binaryDataSet(binaryData);
 	
 };
